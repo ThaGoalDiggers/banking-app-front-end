60717ca7007d6e7ede6c89c1ff5eee41553d74c4
Zachjrivera <Zachjrivera@gmail.com>
2018-12-14 16:58:04 -0500

    added some routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component'
+import { CustomerComponent} from './customer/customer-component/customer.component' 
 
-const routes: Routes = [];
+export const routes: Routes = [
+   {
+     path:'',
+     redirectTo : 'login',
+     pathMatch :'full'
+   },
+   {
+     path : 'login',
+     component : LoginComponent 
+   },
+   {
+     path : 'customer',
+     component : CustomerComponent
+   }
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+
+
+export class AppRoutingModule {
+  
+ 
+ }
