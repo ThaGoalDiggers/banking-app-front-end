e8ee2a3462d6b461e799e62022e5c52266068447
Zachjrivera <Zachjrivera@gmail.com>
2018-12-14 16:03:35 -0500

    start from here

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CustomerComponent } from './customer/customer/customer.component';
+import { AccountComponent } from './account/account/account.component';
+import { BillsComponentComponent } from './bills/bills-component/bills-component.component';
+import { TransactionComponent } from './transaction/transaction/transaction.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CustomerComponent,
+    AccountComponent,
+    BillsComponentComponent,
+    TransactionComponent
   ],
   imports: [
     BrowserModule,
